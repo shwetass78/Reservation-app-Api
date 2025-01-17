0d0b698159741c7266535dd2ecf5e12278c0dc1b
shwetass78 <shwetakumari.ss78@gmail.com>
2025-01-17 14:00:52 +0530

    rooms created sucessfully

--- utils/verifyToken.js
+++ utils/verifyToken.js
@@ -6,8 +6,8 @@ export const verifyToken = (req, res, next) => {
   if (!token) {
     return next(createError(401, "You are not authenticated!"));
   }
-  console.log("token:", token);
-  console.log("secret_key:", process.env.JWT);
+  // console.log("token:", token);
+  // console.log("secret_key:", process.env.JWT);
   jwt.verify(token, process.env.JWT, (err, user) => {
     if (err) return next(createError(403, "Token is not valid!"));
     req.user = user;
